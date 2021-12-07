1a399da7e4314ce2c76e8e00a52a536799e34baa
jck-bit <jckkinyanjui@gmail.com>
2021-12-07 14:03:29 +0300

    The nav-bar

---
+++ src/components/navbar/Menuitems.js
@@ -0,0 +1,27 @@
+ export const Menuitems = [
+    {
+        title:'Home',
+        url:'a',
+        cName:'nav-links'
+    },
+    {
+        title:'services',
+        url:'a',
+        cName:'nav-links'
+    },
+    {
+        title:'Products',
+        url:'a',
+        cName:'nav-links'
+    },
+    {
+        title:'Contact Us',
+        url:'a',
+        cName:'nav-links'
+    },
+    {
+        title:'Home',
+        url:'a',
+        cName:'nav-links'
+    },
+]
