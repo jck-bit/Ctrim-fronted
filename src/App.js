1d89b074e9f4022f70570012077392fe6a8032b4
jck-bit <jckkinyanjui@gmail.com>
2021-12-07 22:25:03 +0300

    got stuck at firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import './App.css';
+import { Container } from 'react-bootstrap';
 import Signup from './components/Login/Signup';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <div className="App">
-      <Signup/>
-    </div>
+    <AuthProvider>
+          <Container 
+     className="d-flex align-items-center justify-content-center"
+    style={{minHeight:"100vh"}}
+    >
+      <div className ="w 100" style={{maxWidth:"700px", minwidth:"700px"}}>
+            <Signup/>
+      </div>
+    </Container>
+    </AuthProvider>
+
+
+    
   );
 }
 
-export default App;
+export default App
--- src/components/Login/Signup.js
+++ src/components/Login/Signup.js
@@ -1,19 +1,28 @@
 import React, {useRef} from 'react'
 import { Form, Button, Card } from 'react-bootstrap'
+import { useAuth } from '../../context/AuthContext'
 
 const Signup = () => {
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
+    const {signup} =useAuth()
+
+ function handleSubmit(e) {
+     e.preventDefault()
+
+     signup(emailRef.current.value, passwordRef.current.value)
+ }
+
     return (
         <>
            <Card>
                <Card.Body>
-                 <h2 className="text-centre mb-4">sign Up </h2>
+                 <h2 className="text-center mb-4">sign Up </h2>
                  <Form>
                     <Form.Group id="email" >
                       <Form.Label>Email</Form.Label>
-                      <Form.Control type ="email" ref ={emailRef} required />
+                      <Form.Control className="w-100" type ="email" ref ={emailRef} required />
                     </Form.Group>
                     <Form.Group id="password" >
                       <Form.Label>password</Form.Label>
@@ -32,7 +41,7 @@ const Signup = () => {
                </Card.Body> 
             </Card> 
            <div className="w-100 text-center mt-2">
-               Alraedy Have an account?
+               Alraedy Have an account? 
            </div>
         </>
     )
