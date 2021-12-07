637f4ab52261c733170bb5086c90ae19e8694016
jck-bit <jckkinyanjui@gmail.com>
2021-12-07 14:17:30 +0300

    progress on the navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import { Menuitems } from './Menuitems'
+import './Navbar.css'
+
+
 const Navbar = () => {
     return (
        <nav className="navbaritems">
            <h1 className="navbar-logo">Ctrim-api</h1>
            <div className="menu-icon"></div>
            <ul>
-              {Menuitems.map((item, index)=>{
+              {Menuitems.map(( item,index)=>{
                   return (
                   <li key ={index}>
-                      <a className={item.cname} href={item.url}>
+                      <a className={item.cName} href={item.url}>
                         {item.title}
                       </a>
                  </li>
               )})}
 
-               <li><a className={tem.cname}></a></li>
            </ul>
        </nav>
     )
